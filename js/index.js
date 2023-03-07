731faa43ff74927771b958b44948c946e9ea154f
Lucas Matheus <matheusalexandre012@gmail.com>
2023-03-07 14:52:18 -0300

    perguntas esconder texto

--- js/index.js
+++ js/index.js
@@ -17,7 +17,6 @@ links.forEach(ativarLink);
 // ativar items do orçamento
 
 const parametros = new URLSearchParams(location.search); 
-console.log(parametros);
 function ativarProduto (parametro){
     
     const elemento = document.getElementById(parametro); 
@@ -28,3 +27,23 @@ function ativarProduto (parametro){
 }
 
 parametros.forEach(ativarProduto);
+
+
+// perguntas frequentes
+
+const perguntas = document.querySelectorAll(".perguntas button"); 
+
+function ativarPergunta(event){
+    const pergunta = event.currentTarget;
+    const controls = pergunta.getAttribute("aria-controls"); 
+    const resposta = document.getElementById(controls);
+
+    resposta.classList.toggle("ativa");
+    const ativa = resposta.classList.contains("ativa")
+    pergunta.setAttribute('aria-expanded', ativa);
+}
+function eventosPerguntas(pergunta){
+    pergunta.addEventListener('click', ativarPergunta)
+}
+
+perguntas.forEach(eventosPerguntas)
